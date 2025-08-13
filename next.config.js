b1ac7ee979c1a5d74d84fa23c16f80a51467db00
malikarslan <malik.arslan@discretelogix.com>
2025-08-13 06:42:48 +0500

    config file modified

--- next.config.js
+++ next.config.js
@@ -9,7 +9,15 @@ const nextConfig = {
     reactStrictMode: true,
     allowedDevOrigins: [
         '192.168.1.84'
-    ]
+    ],
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*', // anything starting with /api
+                destination: 'http://3.130.68.140:6001/api/:path*', // your EC2 backend
+            },
+        ];
+    }
 };
 
 module.exports = nextConfig;
